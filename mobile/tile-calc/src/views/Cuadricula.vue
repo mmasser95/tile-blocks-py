17dbb8026021186cc067b045c2cf12bb0b7a92f4
mmasser95 <mmasser95@gmail.com>
2025-02-08 16:34:53 +0100

    TASK: Cuadricula

--- mobile/tile-calc/src/views/Cuadricula.vue
+++ mobile/tile-calc/src/views/Cuadricula.vue
@@ -10,7 +10,12 @@
                     <ion-input label="Tile Width (cm)" label-placement="floating" type="number" v-model="tileWidth" />
                     <ion-input label="Tile Height (cm)" label-placement="floating" type="number" v-model="tileHeight" />
                 </div>
-                <v-stage :config="stageConfig">
+                <div class="h-flex">
+                    <p>Desperdicio X: {{ spareX }}x{{ spareY }} cm</p>
+                    <p>Baldosas (enteras): {{ complete_x }}x{{ complete_y }}</p>
+                    <p>Baldosas (todas): {{ tilesX }}x{{ tilesY }}</p>
+                </div>
+                <v-stage ref="stage" :config="stageConfig">
                     <v-layer>
                         <v-rect v-for="(tile, index) in tiles" :key="index" :config="{
                             x: tile.x,
@@ -23,7 +28,7 @@
                     </v-layer>
                 </v-stage>
             </div>
-            <div v-if="tileInfo" class="tile-info">
+            <div v-if="tileInfo" class="h-flex">
                 <p><strong>Medidas de la baldosa:</strong></p>
                 <p>Ancho: {{ tileInfo.w }}cm</p>
                 <p>Alto: {{ tileInfo.h }}cm</p>
@@ -36,12 +41,22 @@
 <script lang="ts" setup>
 import { IonContent, IonPage, IonInput, IonGrid, IonRow } from '@ionic/vue';
 import Layout from '@/components/Layout.vue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const areaWidth = ref(150);
 const areaHeight = ref(170)
 const tileWidth = ref(20)
 const tileHeight = ref(30)
+
+const spareX = ref(0)
+const spareY = ref(0)
+
+const complete_x = ref(0)
+const complete_y = ref(0)
+
+const tilesX = ref(0)
+const tilesY = ref(0)
+const stage = ref()
 
 type Tile = {
     x: number,
@@ -54,14 +69,20 @@ const tiles = ref<Tile[]>([])
 
 const tileInfo = ref<Tile>()
 
-const stageConfig=ref({
-    width:window.innerWidth,
-    height:window.innerHeight/4 + window.innerHeight/3
+const stageConfig = ref({
+    width: window.innerWidth,
+    height: window.innerHeight / 4 + window.innerHeight / 3
 })
 
 const calcTiles = () => {
     const tiles_x = Math.ceil(areaWidth.value / tileWidth.value)
     const tiles_y = Math.ceil(areaHeight.value / tileHeight.value)
+    tilesX.value = tiles_x
+    tilesY.value = tiles_y
+    spareX.value = areaWidth.value % tileWidth.value
+    spareY.value = areaHeight.value % tileHeight.value
+    complete_y.value = Math.floor(areaHeight.value / tileHeight.value)
+    complete_x.value = Math.floor(areaWidth.value / tileWidth.value)
     let result = []
     for (let i = 0; i < tiles_x; i++) {
         for (let j = 0; j < tiles_y; j++) {
@@ -82,20 +103,39 @@ const calcTiles = () => {
         }
     }
     tiles.value = result
+    stageConfig.value = {
+        width: tiles_x * tileWidth.value,
+        height: tiles_y * tileHeight.value,
+    };
 }
 
 const showTileInfo = (tile: Tile) => {
     tileInfo.value = tile
 }
 
+
+
 calcTiles()
+
+watch(areaHeight, () => calcTiles())
+watch(areaWidth, () => calcTiles())
+watch(tileHeight, () => {
+    //@ts-ignore
+    tileHeight.value = parseInt(tileHeight.value)
+    calcTiles()
+})
+watch(tileWidth, () => {
+    //@ts-ignore
+    tileWidth.value = parseInt(tileWidth.value)
+    calcTiles()
+})
 
 </script>
 <style scoped>
 .h-flex {
     display: flex;
     justify-content: space-between;
-    gap: 5px;
+    gap: 10px;
 }
 
 .v-flex {
